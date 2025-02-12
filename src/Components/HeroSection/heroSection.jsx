061f66f29c1af7948224f724a61d05f40d3a6535
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-02-12 04:46:11 -0800

    hero-section

--- src/Components/HeroSection/heroSection.jsx
+++ src/Components/HeroSection/heroSection.jsx
@@ -3,11 +3,10 @@ import './heroSection.css'
 export default function HeroSection() {
     return (
         <section className="text-gray-600 body-font section-one">
-            <div className="container px-20 py-10 mx-auto flex md:flex-row flex-col items-center section-one-childOne">
+            <div className="container lg:px-20 py-10 mx-auto flex md:flex-row flex-col items-center section-one-childOne">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                        AI-Powered OMR Grading System
-                        <br className="hidden lg:inline-block" />
+                    <h1 className="title-font sm:text-3xl text-2xl mb-4 font-medium text-gray-900">
+                        AI-Powered OMR Grading System 
                     </h1>
                     <p className="mb-8 leading-relaxed">
                         Upload your OMR sheet and let AI grade it instantly with high accuracy!
