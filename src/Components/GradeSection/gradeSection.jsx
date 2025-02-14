a489261a3671c0b834ed5bd6d7f0bf5d2280a37c
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-02-13 23:42:26 -0800

    gradeSection practice

---
+++ src/Components/GradeSection/gradeSection.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function GradeSection() {
+    return (
+        <section className="text-gray-600 body-font">
+            <div className="container lg:px-20 py-10 mx-auto flex md:flex-row gap-10 flex-col items-center GradeSection">
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mt-4 GradeSection-childOne">
+                    <div className="gradeImg"></div>
+                </div>
+                <div className="lg:flex-grow md:w-1/2 lg:pr-2 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center GradeSec">
+                    <h1 className="title-font sm:text-3xl lg:text-4xl text-2xl mb-4 font-medium text-[#003C46]">
+                        Deliver and Grade Your Assessments Anywhere
+                    </h1>
+                    <p className="mb-8 leading-relaxed mt-2">
+                        Gradescope helps you seamlessly administer and grade all of your assessments,
+                        whether online or in-class. Save time grading and get a clear picture of how your
+                        students are doing.
+                    </p>
+                    <div className="flex justify-center">
+                        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+                           Get Started
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default GradeSection
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import HeroSection from '../../Components/HeroSection/heroSection'
 import ExamSection from '../../Components/ExamSection/examSection'
 import PictureSection from '../../Components/PictureCaptureSection/pictureSection'
+import GradeSection from '../../Components/GradeSection/gradeSection'
 
 function Home() {
     return (
         <>
+            <GradeSection />
             <HeroSection />
             <ExamSection />
             <PictureSection />
