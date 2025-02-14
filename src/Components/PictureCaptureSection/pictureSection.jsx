8487a564ccb65da4e21c3d563133b2f627e049ef
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-02-14 02:35:45 -0800

    picture caption

--- src/Components/PictureCaptureSection/pictureSection.jsx
+++ src/Components/PictureCaptureSection/pictureSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PictureSection() {
     return (
         <section className="text-gray-600 body-font">
-            <div className="container lg:px-20 py-10 mx-auto flex md:flex-row gap-10 flex-col items-center">
+            <div className="container lg:px-20 py-10 mx-auto flex md:flex-row flex-col items-center">
                 <div className="lg:flex-grow relative bottom-8 md:w-1/2 lg:pr-2 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center PictureSec">
                     <h1 className="title-font sm:text-3xl lg:text-4xl mb-4 font-medium text-[#3D31B9]">
                         Picture Capture
@@ -20,7 +20,7 @@ function PictureSection() {
                         </button>
                     </div>
                 </div>
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mt-4 ExamSection-childOne">
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mt-4 PicSection-childTwo">
                     <div className="pictureImg"></div>
                 </div>
             </div>
