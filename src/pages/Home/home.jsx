aa1bfd4a2dd9da985488bb75a112509aa3732353
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-02-14 03:15:43 -0800

    footer secton and rsponsive

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -3,6 +3,7 @@ import HeroSection from '../../Components/HeroSection/heroSection'
 import ExamSection from '../../Components/ExamSection/examSection'
 import PictureSection from '../../Components/PictureCaptureSection/pictureSection'
 import GradeSection from '../../Components/GradeSection/gradeSection'
+import Footer from '../../Components/Footer/footer'
 
 function Home() {
     return (
@@ -11,6 +12,7 @@ function Home() {
             <HeroSection />
             <ExamSection />
             <PictureSection />
+            <Footer />
         </>
     )
 }
