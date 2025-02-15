5101c7cc32bba1f6b4e16248487216907f79ecb1
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-02-15 00:10:51 -0800

    home

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -11,6 +11,7 @@ function Home() {
             <GradeSection />
             <HeroSection />
             <ExamSection />
+            {/* <MainS  */}
             <PictureSection />
             <Footer />
         </>
